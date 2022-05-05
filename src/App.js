ba9fcbbecfb6df0591cb1aca8fa5d152764534d5
sunpark <yangheat@gmail.com>
2022-05-06 00:11:15 +0900

    ~lesson 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [logo, setLogo] = useState("BLOG REACT");
+  let [title, setTitle] = useState([
+    "POST TITLE1",
+    "POST TITLE2",
+    "POST TITLE3",
+  ]);
+  let [like, setLike] = useState(0);
+
+  function likeClick() {
+    setLike(like + 1);
+  }
+
+  function titleChange() {
+    setTitle(["POST TITLE CHANGE", title[1], title[2]]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigator">
+        <h4>{logo}</h4>
+      </div>
+      <div className="post-list">
+        <div className="post">
+          <h4>
+            {title[0]}&nbsp;
+            <span onClick={likeClick}>👍&nbsp;</span>
+            {like}
+          </h4>
+          <button onClick={titleChange}>TITLE CAHNGE</button>
+          <p>register date</p>
+        </div>
+        <div className="post">
+          <h4>{title[1]}</h4>
+          <p>register date</p>
+        </div>
+        <div className="post">
+          <h4>{title[2]}</h4>
+          <p>register date</p>
+        </div>
+      </div>
     </div>
   );
 }
